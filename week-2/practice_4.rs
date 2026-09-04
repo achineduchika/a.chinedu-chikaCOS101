08c3aac1d5f88199c61af6a761330e6b9975ea39
Angel Chinedu-Chika <a.chinedu-chika@pau.edu.ng>
2026-09-04 03:12:44 -0700

    My second commit

---
+++ week-2/practice_4.rs
@@ -0,0 +1,12 @@
+fn main() {
+   let p:f64 = 1000.0;
+   let r:f64 = 1.0;
+   let t:f64 = 2.0;
+
+   // simple interst
+   let a = p * ( r/100.0) * t;
+   println!("Amount is {}", a);
+   let si = a - p;
+   println!("Simple Interest is {}", si);
+
+}
