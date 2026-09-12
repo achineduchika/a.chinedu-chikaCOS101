214f89fa11bc282888e9add118f6b4ffdc1ba6b8
Chinedu-Chika Angel Mmesoma <angel.chinedu-chika@pau.edu.ng>
2026-09-12 21:35:00 +0100

    Testing of Data Types

---
+++ week-3/practice_5/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+   let special_character = '@'; //default
+   let alphabet:char = 'B';
+   let surname = "Chinedu-Chika";
+   let first_name = "Angel";
+   let middle_name = "Mmesoma";
+
+   println!(" ");
+   println!("Special character: {}",special_character);
+   println!("Alphabet: {}",alphabet);
+   println!("Name: {} {} {}",surname, middle_name, first_name);
+}
